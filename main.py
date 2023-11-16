fix time calculation for different strategies in the simulation

main.py
@@ -5,31 +5,47 @@ import pickle
 from lib import AerDeployment, BasicOffloading
 
 
-def compute(ns_UAV, ns_HAP, eps_all, nu_all, file_name):
+def compute(n_ues, ns_UAV, ns_HAP, eps_all, nu_all, file_name, strategy_number):
     # create and configure deployment
-    deployment = AerDeployment()
+    deployment = AerDeployment(n_ues)
     deployment.drop_nodes()
     deployment.compute_channels()
 
     perc_stat = []
     x, y = [], []
     for eps_val in eps_all:
-        for nu_val in nu_all:
-            # create the offloading instance
-            offl = BasicOffloading(deployment.conn_info, 10000, 2, deployment.n_uav,
-                                   eps_val, nu_val, ns_UAV, ns_HAP, 1)
+        if strategy_number == 1:
+            for nu_val in nu_all:
+                # create the offloading instance
+                offl = BasicOffloading(deployment.conn_info, 1000, 2, deployment.n_uav,
+                                       eps_val, nu_val, ns_UAV, ns_HAP, strategy_number)
+                offl.run()
+                perc_stat.append(offl.percent_below_thr)
+                x.append(eps_val)
+                y.append(nu_val)
+        elif strategy_number == 4:
+            offl = BasicOffloading(deployment.conn_info, 1000, 2, deployment.n_uav,
+                                   1.0, 0.0, ns_UAV, ns_HAP, 1)
+            offl.run()
+            perc_stat.append(offl.percent_below_thr)
+            x.append(1.0)
+            y.append(0.0)
+        else:
+            offl = BasicOffloading(deployment.conn_info, 100, 2, deployment.n_uav,
+                                   eps_val, 0.1, ns_UAV, ns_HAP, strategy_number)
             offl.run()
             perc_stat.append(offl.percent_below_thr)
             x.append(eps_val)
-            y.append(nu_val)
+            for nu_val in nu_all:
+                y.append(nu_val)
 
-    data = {'x': x,
-            'y': y,
-            'z': perc_stat}
+        data = {'x': x,
+                'y': y,
+                'z': perc_stat}
 
-    with open(file_name, 'wb') as file:
-        # Serialize and write the variable to the file
-        pickle.dump(data, file)
+        with open(file_name, 'wb') as file:
+            # Serialize and write the variable to the file
+            pickle.dump(data, file)
 
 
 def plot_from_data(file_name):
@@ -53,10 +69,12 @@ def plot_from_data(file_name):
 
 if __name__ == '__main__':
 
+    n_ues = 30
     ns_UAV = 5
     ns_HAP = 15
+    strategy_number = 2
     eps_all = np.linspace(0, 1, 11)
     nu_all = np.linspace(0, 1, 11)
-    file_name = 'data_30_1st_r2.pickle'
-    compute(ns_UAV, ns_HAP, eps_all, nu_all, file_name)
+    file_name = 'data_'+str(n_ues)+'_str'+str(strategy_number)+'_r2.pickle'
+    compute(n_ues, ns_UAV, ns_HAP, eps_all, nu_all, file_name, strategy_number)
     plot_from_data(file_name)